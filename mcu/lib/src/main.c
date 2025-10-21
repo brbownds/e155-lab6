b4abab680a4e7ddcbc6b8b2ce011440f42825690
brbownds <brbownds@g.hmc.edu>
2025-10-21 15:32:08 -0700

    added more files

--- mcu/lib/src/main.c
+++ mcu/lib/src/main.c
@@ -2,43 +2,53 @@
 // Broderick Bownds
 // brbownds@hmc.edu
 // 10/20/2025
-
+//
+// This program controls an LED and a DS1722 temperature sensor over SPI,
+// responding to web requests via the ESP8266.
 #include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
+#include "STM32L432KC_GPIO.h"
+#include "STM32L432KC_SPI.h"
+#include "STM32L432KC_TIM.h"
+#include "DS1722.h"
 
-/////////////////////////////////////////////////////////////////
-// Provided Constants and Functions
-/////////////////////////////////////////////////////////////////
+// HTML content
+char* webpageStart =
+"<!DOCTYPE html><html><head><title>E155 Web Server</title>\
+<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"></head>\
+<body><h1>Broderick Bownds: E155 IoT Temperature Server</h1>";
 
-//Defining the web page in two chunks: everything before the current time, and everything after the current time
-char* webpageStart = "<!DOCTYPE html><html><head><title>E155 Web Server Demo Webpage</title>\
-	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
-	</head>\
-	<body><h1>E155 Web Server Demo Webpage</h1>";
-char* ledStr = "<p>LED Control:</p><form action=\"ledon\"><input type=\"submit\" value=\"Turn the LED on!\"></form>\
-	<form action=\"ledoff\"><input type=\"submit\" value=\"Turn the LED off!\"></form>";
+char* ledStr =
+"<p>LED Control:</p>\
+<form action=\"ledon\"><input type=\"submit\" value=\"Turn LED On!\"></form>\
+<form action=\"ledoff\"><input type=\"submit\" value=\"Turn LED Off!\"></form>";
 
-// char* tempstr
+char* tempForm =
+"<p>Temperature Resolution:</p>\
+<form action=\"precision8\"><input type=\"submit\" value=\"8-bit\"></form>\
+<form action=\"precision9\"><input type=\"submit\" value=\"9-bit\"></form>\
+<form action=\"precision10\"><input type=\"submit\" value=\"10-bit\"></form>\
+<form action=\"precision11\"><input type=\"submit\" value=\"11-bit\"></form>\
+<form action=\"precision12\"><input type=\"submit\" value=\"12-bit\"></form>";
 
-char* webpageEnd   = "</body></html>";
+char* webpageEnd = "</body></html>";
 
-//determines whether a given character sequence is in a char array request, returning 1 if present, -1 if not present
+// Checks if a substring exists inside a string
 int inString(char request[], char des[]) {
-	if (strstr(request, des) != NULL) {return 1;}
+	if (strstr(request, des) != NULL) return 1;
 	return -1;
 }
 
-int updateLEDStatus(char request[])
-{
+// Handles LED on/off requests
+int updateLEDStatus(char request[]) {
 	int led_status = 0;
-	// The request has been received. now process to determine whether to turn the LED on or off
-	if (inString(request, "ledoff")==1) {
+
+	if (inString(request, "ledoff") == 1) {
 		digitalWrite(LED_PIN, PIO_LOW);
 		led_status = 0;
-	}
-	else if (inString(request, "ledon")==1) {
+	} 
+	else if (inString(request, "ledon") == 1) {
 		digitalWrite(LED_PIN, PIO_HIGH);
 		led_status = 1;
 	}
@@ -46,54 +56,74 @@ int updateLEDStatus(char request[])
 	return led_status;
 }
 
-void newprecision(char request[])
-{
-    int precision = 9; // default
+// Handles DS1722 precision requests
+void newprecision(char request[]) {
+	int precision = 9;
 
-    if (inString(request, "precision8") == 1) {
-        precision = 8;
-    } 
-    else if (inString(request, "precision9") == 1) {
-        precision = 9;
-    } 
-    else if (inString(request, "precision10") == 1) {
-        precision = 10;
-    } 
-    else if (inString(request, "precision11") == 1) {
-        precision = 11;
-    } 
-    else if (inString(request, "precision12") == 1) {
-        precision = 12;
-    }
+	if (inString(request, "precision8") == 1) precision = 8;
+	else if (inString(request, "precision9") == 1) precision = 9;
+	else if (inString(request, "precision10") == 1) precision = 10;
+	else if (inString(request, "precision11") == 1) precision = 11;
+	else if (inString(request, "precision12") == 1) precision = 12;
 
-    setprecision(precision);
+	setPrecision(precision);
 }
-/////////////////////////////////////////////////////////////////
-// Solution Functions
-/////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////
+// Main Function
+/////////////////////////////////////////////////////////////////
 int main(void) {
-  configureFlash();
-  configureClock();
+	configureFlash();
+	configureClock();
 
-  gpioEnable(GPIO_PORT_A);
-  gpioEnable(GPIO_PORT_B);
-  gpioEnable(GPIO_PORT_C);
+	gpioEnable(GPIO_PORT_A);
+	gpioEnable(GPIO_PORT_B);
+	gpioEnable(GPIO_PORT_C);
 
-  pinMode(PB3, GPIO_OUTPUT);
-  
-  RCC->APB2ENR |= (RCC_APB2ENR_TIM15EN);
-  initTIM(TIM15);
-  
-  // TODO: Add SPI initialization code
+	pinMode(LED_PIN, GPIO_OUTPUT);
 
-  while(1) {
-  
+	RCC->APB2ENR |= RCC_APB2ENR_TIM15EN;
+	initTIM(TIM15);
 
+	// Initialize SPI for DS1722: CPOL=0, CPHA=1
+	initSPI(0b111, 0, 1);
+	setPrecision(12); // Default: highest resolution
 
-    // TODO: Add SPI code here for reading temperature
-  
-    // Update string with current LED state
-  
-   
+	USART_TypeDef *USART = initUSART(USART1_ID, 125000);
+	printf("E155 IoT SPI Temperature Server Initialized\n");
+
+	while (1) {
+		// Wait for ESP8266 request
+		char request[64] = " ";
+		int index = 0;
+
+		while (inString(request, "\n") == -1) {
+			while (!(USART->ISR & USART_ISR_RXNE));
+			request[index++] = readChar(USART);
+		}
+
+		// Process web request
+		int led_status = updateLEDStatus(request);
+		newprecision(request);
+		float temp = getTemp();
+
+		// Prepare status messages
+		char ledMsg[32];
+		if (led_status) sprintf(ledMsg, "LED is ON");
+		else sprintf(ledMsg, "LED is OFF");
+
+		char tempMsg[48];
+		sprintf(tempMsg, "Temperature: %.3f °C", temp);
+
+		// Send the full webpage back
+		sendString(USART, webpageStart);
+		sendString(USART, ledStr);
+		sendString(USART, tempForm);
+		sendString(USART, "<h2>Status</h2><p>");
+		sendString(USART, ledMsg);
+		sendString(USART, "<br>");
+		sendString(USART, tempMsg);
+		sendString(USART, "</p>");
+		sendString(USART, webpageEnd);
+	}
 }
